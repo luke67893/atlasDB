45b3d8a5c640d8b8d7fb1c007d626163270dd482
Conrad <grosserconrad@gmail.com>
2018-02-07 00:16:46 +0100

    Changelog Update 1
    - Added app to the server
    - Added posting tool to the admin interface
    - Created 2 views for the changelogs
    - Adjusted routes for correct routing

--- taskDB/changelog/views.py
+++ taskDB/changelog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# View for the overview over the changelog
+def index(request):
+	return HttpResponse("Hello World")
+
+# View for each post
+def post(request, ChangelogPost_id):
+	return HttpResponse("You are looking at post %s" % ChangelogPost_id)
