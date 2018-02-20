e90a391e8caa4c5bfc2f8ed1807ae241f3e1090d
Conrad <grosserconrad@gmail.com>
2018-02-20 19:40:49 +0100

    Started upload function (with django)
    Started to integrate the uploding capabilities of django (django filesystem)

--- taskDB/atlasUpload/views.py
+++ taskDB/atlasUpload/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from taskDB.atlasUpload.models import Task
+from taskDB.atlasUpload.forms import DocumentForm
 
 # Create your views here.
 
 def upload(request):
-    return render(request, 'atlasUpload/index.html')
+	# Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('atlasUpload.views.list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render_to_response(
+        'atlasUpload/index.html',
+        {'documents': documents, 'form': form},
+        context_instance=RequestContext(request)
+    )
+
+    # return render(request, 'atlasUpload/index.html')
--- taskDB/taskDB/settings.py
+++ taskDB/taskDB/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'PRODUCTIONKEY'
 
+# MEDIA References
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
