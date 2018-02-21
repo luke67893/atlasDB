366085d6a8fd4c7a858f6e6db5ebb3906672eca8
Conrad <grosserconrad@gmail.com>
2018-02-21 12:25:38 +0100

    Deleted atlasUpload app
    - Removed all data for the atlasUpload App for reworking

--- taskDB/atlasUpload/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class UploadForm(forms.Form):
-	aufgabenname = forms.CharField(max_length=255)
-	lehrer = forms.CharField(max_length=255)
-	uploaddate = forms.DateTimeField()
-	docfile = forms.FileField()
-	
