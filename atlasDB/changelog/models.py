594b852f3d3721f8652c716250b7651b62c257a3
Conrad <grosserconrad@gmail.com>
2018-02-21 14:44:34 +0100

    Cleanup 1
    Project reset to initial state

--- atlasDB/changelog/models.py
+++ atlasDB/changelog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
-# changelog post
+from datetime import datetime
 
 class ChangelogPost(models.Model):
 	title = models.CharField(max_length=255)
 	content = models.TextField()
-	date = models.DateTimeField('Date published')
+	date = models.DateTimeField('Date published', default=datetime.now)
 	author = models.TextField()
 	# This is just for showing the title in the admin interface
 	def __str__(self):
