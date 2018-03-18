2d83d498d970c7500c8330cd6af558dc75fd53a5
Conrad <grosserconrad@gmail.com>
2018-03-18 20:40:11 +0100

    Major Design Update 2
    - Updated Error page (X1)
    - Restructured HTML for templating
    - Tested download page

--- atlasDB/atlas/urls.py
+++ atlasDB/atlas/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
 	path('', views.upload),
 	path('upload/', views.upload, name='upload'),
 	path('upload/success/', views.success, name='success'),
-	path('upload/error/', views.error, name='error'),
+	path('upload/errorx1/', views.errorx1, name='errorx1'),
 	path('dashboard/', views.dashboard, name='dashboard'),
 	path('aufgabe/<int:id>/', views.aufgabe, name='aufgabe'),
+	path('download/<int:id>/', views.download, name='download'),
 ]
