a34c6414d2659e43ca3eb6ecfb3f9c1c65f6ec6e
Conrad <grosserconrad@gmail.com>
2018-04-14 20:28:40 +0200

    Created custom login form
    - Created custom login form
    - Added standard login and logout functionality
    - Added content to the main_template
    - Tested restrictions of methods

--- atlasDB/userinterface/views.py
+++ atlasDB/userinterface/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from .models import Task
 
 def welcome(request):
     return(HttpResponse("Hello World."))
 
 def login(request):
-    return(HttpResponse("Login Page."))
+    return(render(request, 'userinterface/main_template.html'))
 
 def logout(request):
     return(HttpResponse("Logout Page."))
+
+@login_required
+def main(request):
+    # my_tasks = Task.objects.all()
+    return(HttpResponse("Shouldnt see this"))
