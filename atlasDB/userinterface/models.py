4a370ced3b5800036ad2efbb15302a4e45691644
Conrad <grosserconrad@gmail.com>
2018-04-14 19:18:14 +0200

    Model Correction
    - Adjusted models
    - Added custom URLs config

--- atlasDB/userinterface/models.py
+++ atlasDB/userinterface/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Subject(models.Model):
-    subject_name = models.CharField(max_lenght=50)
+    subject_name = models.CharField(max_length=25)
 
 class Stage(models.Model):
-    stage_level = models.IntegerField(validators=[MaxValueValidator(13), MinValueValidator(1)])
+    stage_level = models.IntegerField(validators=[MaxValueValidator(13),MinValueValidator(1)]
+     )
 
 class Task(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     last_changed_at = models.DateTimeField(auto_now=True)
-    task_name = models.CharField(max_lenght=255)
+    task_name = models.CharField(max_length=255)
     document = models.FileField(upload_to='Tasks/%Y/%m')
-    teacher = models.ForeignKey(User, related_name="teacher")
-    subject = models.ForeignKey(Subject)
+    teacher = models.ForeignKey(User, related_name="teacher", on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
