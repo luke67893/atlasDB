eeb86ed9c78df3a73c703d16eba864f40e6913ce
Conrad <grosserconrad@gmail.com>
2018-04-25 15:26:25 +0200

    Updated login system
    - Updated user handling
    - Restyled main template
    - Created home page for users
    - Added redirects
    - Added url path for the home
    - Updated login form
    - Cleared out custom CSS in favor of materialize

--- atlasDB/userinterface/urls.py
+++ atlasDB/userinterface/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
 	path('login/', LoginView.as_view(template_name="userinterface/login_form.html"), name="user_login"),
 	path('logout/', LogoutView.as_view(), name="user_logout"),
-	path('main/', views.main, name="main"),
-	path('', views.welcome, name="welcome_page"),
+	path('home/', views.user_home, name="user_home"),
+	path('', views.welcome, name="welcome"),
 ]
--- atlasDB/userinterface/views.py
+++ atlasDB/userinterface/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.conf.urls import url
 
 from .models import Task
 
 def welcome(request):
-    return(render(request, 'userinterface/main_template.html'))
+	if request.user.is_authenticated:
+		return redirect('user_home')
+	return(render(request, 'userinterface/main_template.html'))
 
-def login(request):
-    return(render(request, 'userinterface/login_form.html'))
+def user_login(request):
+	return(render(request, 'userinterface/login_form.html'))
 
-def logout(request):
-    return(HttpResponse("Logout Page."))
+def user_logout(request):
+	return(HttpResponse("Logout Page."))
 
 @login_required
-def main(request):
-    # my_tasks = Task.objects.all()
-    return(HttpResponse("Shouldnt see this"))
+def user_home(request):
+	# my_tasks = Task.objects.all()
+	return(render(request, 'userinterface/home.html'))
