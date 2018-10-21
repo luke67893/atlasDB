8f5b3a2d2b16dcfd750d68294d0af14af1d42d43
Conrad Großer <grosserconrad@gmail.com>
2018-10-21 22:44:15 +0200

    Changed to a local database
    - Minor code improvements

--- atlasDB/atlasDB/settings.py
+++ atlasDB/atlasDB/settings.py
@@ -62,15 +62,10 @@ WSGI_APPLICATION = 'atlasDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'atlasdb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
