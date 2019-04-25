92b488c12faa6ac4d9d960dfe9515dc5eaa5a80f
Conrad <grosserconrad@gmail.com>
2019-04-25 14:09:45 +0200

    Code Cleanup
    - Fixed bug with uploading
    - Fixed migrations bug
    - Converted all static urls to dynamic urls
    - Improved forms to dynamic forms
    - Added custom ids for subjects and stages
    - Added stability to downloader
    - Other stability and performance improvements
    - Subjects have unique names now
    - Improved file upload by checking if the form is valid and using ModelChoiceFields instead of ChoiceFields

--- atlasDB/userinterface/models.py
+++ atlasDB/userinterface/models.py
@@ -3,14 +3,17 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Subject(models.Model):
-    subject_name = models.CharField(max_length=25)
+    subject_id = models.AutoField(primary_key=True)
+    subject_name = models.CharField(max_length=25, unique=True)
+
     def __str__(self):
         return self.subject_name
 
 class Task(models.Model):
+    task_id = models.AutoField(primary_key=True)
+    task_name = models.CharField(max_length=255)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     last_changed_at = models.DateTimeField(auto_now=True)
-    task_name = models.CharField(max_length=255)
     document = models.FileField(upload_to='Tasks/%Y/%m')
     teacher = models.ForeignKey(User, related_name="teacher", on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING, null=True, blank=True)
--- atlasDB/userinterface/views.py
+++ atlasDB/userinterface/views.py
@@ -10,166 +10,201 @@ from .forms import UploadForm
 import os
 from django.conf import settings
 
+
 def user_logout(request):
-	return HttpResponse("Logout Page.")
+    return HttpResponse('Logout Page.')
+
 
 def welcome(request):
-	if request.user.is_authenticated:
-		return redirect('user_home')
-	context = {
-		'ptitle': "Welcome to atlasDB",
-		'pheadline': "Hi there!"
-	}
-	return render(request, 'userinterface/main.html', context)
+    if request.user.is_authenticated:
+        return redirect('user_home')
+    context = {
+        'ptitle': 'Welcome to atlasDB',
+        'pheadline': 'Hi there!'
+    }
+    return render(request, 'userinterface/main.html', context)
+
 
 def user_login(request):
-	context = {
-		'ptitle': "Login to atlasDB",
-		'pheadline': "Login to atlasDB"
-	}
-	return render(request, 'userinterface/login_form.html', context)
+    context = {
+        'ptitle': 'Login to atlasDB',
+        'pheadline': 'Login to atlasDB'
+    }
+    return render(request, 'userinterface/login_form.html', context)
+
 
 def help(request):
-	context = {
-		'ptitle': "Help",
-		'pheadline': "Help for atlasDB"
-	}
-	return render(request, 'userinterface/help.html', context)
+    context = {
+        'ptitle': 'Help',
+        'pheadline': 'Help for atlasDB'
+    }
+    return render(request, 'userinterface/help.html', context)
+
 
 def impressum(request):
-	context = {
-		'ptitle': "Impressum",
-		'pheadline': "Impressum"
-	}
-	return render(request, 'userinterface/impressum.html', context)
+    context = {
+        'ptitle': 'Impressum',
+        'pheadline': 'Impressum'
+    }
+    return render(request, 'userinterface/impressum.html', context)
+
 
 @login_required
 def user_home(request):
-	context = {
-		'tasks': Task.objects.all(),
-		'ptitle': "Home - " + request.user.username,
-		'pheadline': "All tasks"
-	}
-	return render(request, 'userinterface/taskview.html', context)
+    context = {
+        'tasks': Task.objects.all(),
+        'ptitle': 'Home - ' + request.user.username,
+        'pheadline': 'All tasks'
+    }
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
 def my_tasks(request):
-	my_tasks = Task.objects.filter(teacher=request.user)
-	context = {
-		'tasks': my_tasks,
-		'tasknumber': len(my_tasks),
-		'ptitle': "My Tasks",
-		'pheadline': "Your tasks (" + request.user.username + ")"
-	}
-	return render(request, 'userinterface/taskview.html', context)
+    my_tasks = Task.objects.filter(teacher=request.user)
+    context = {
+        'tasks': my_tasks,
+        'tasknumber': len(my_tasks),
+        'ptitle': 'My Tasks',
+        'pheadline': 'Your tasks (' + request.user.username + ')'
+    }
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
 def upload(request):
-	if request.method == "POST":
-		new_task = Task(teacher=request.user, task_name=request.POST['task_name'], subject=Subject.objects.get(subject_name=request.POST['subject']), stage=request.POST['stage'], document=request.FILES['document'])
-		new_task.save()
-		return redirect('user_home')
-	context = {
-		'form': UploadForm(),
-		'ptitle': "Upload",
-		'pheadline': "Upload"
-	}
-	return render(request, 'userinterface/upload.html', context)
+    form = UploadForm()
+
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            Task.objects.get_or_create(
+                teacher=request.user,
+                task_name=form.cleaned_data['task_name'],
+                subject=form.cleaned_data['subject'],
+                stage=form.cleaned_data['stage'],
+                document=request.FILES['document']
+                )
+            return redirect('user_home')
+
+    context = {
+        'form': form,
+        'ptitle': 'Upload',
+        'pheadline': 'Upload'
+    }
+    return render(request, 'userinterface/upload.html', context)
+
 
 @login_required
 def searchoverview(request):
-	context = {
-		'ptitle': "Search",
-		'pheadline': "Search a task"
-	}
-	return render(request, 'userinterface/searchbar.html', context)
+    context = {
+        'ptitle': 'Search',
+        'pheadline': 'Search a task'
+    }
+    return render(request, 'userinterface/searchbar.html', context)
+
 
 @login_required
 def search(request, keyword):
-	results = Task.objects.filter(task_name__contains=keyword)
-	if len(results) == 0:
-		raise Http404("No tasks found.")
-	context = {
-		'ptitle': "Search: " + str(keyword),
-		'pheadline': "Search: " + str(keyword),
-		'tasks': results
-	}
-	return render(request, 'userinterface/taskview.html', context)
+    results = Task.objects.filter(task_name__contains=keyword)
+    if len(results) == 0:
+        raise Http404('No tasks found.')
+    context = {
+        'ptitle': 'Search: ' + str(keyword),
+        'pheadline': 'Search: ' + str(keyword),
+        'tasks': results
+    }
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
 def stages(request):
-	stages = []
-	[ stages.append(i) for i in range(1,14) ]
-	context = {
-		'ptitle': "Stage Overview",
-		'pheadline': "All stages",
-		'stages': stages
-	}
-	return render(request, 'userinterface/overview.html', context)
+    stages = []
+    [stages.append(i) for i in range(1, 14)]
+    context = {
+        'ptitle': 'Stage Overview',
+        'pheadline': 'All stages',
+        'stages': stages
+    }
+    return render(request, 'userinterface/overview.html', context)
+
 
 @login_required
 def stage(request, stagenumber):
-	taskfilter = Task.objects.filter(stage=stagenumber) 
-	context = {
-		'stage': stagenumber,
-		'tasks': taskfilter,
-		'ptitle': "Filter: Stage " + str(stagenumber),
-		'pheadline': "Filter: Stage " + str(stagenumber),
-		'subjects': Subject.objects.all(),
-		'tasknumber': len(taskfilter)
-	}
-	return render(request, 'userinterface/taskview.html', context)
+    taskfilter = Task.objects.filter(stage=int(stagenumber))
+    context = {
+        'stage': stagenumber,
+        'tasks': taskfilter,
+        'ptitle': 'Filter: Stage ' + str(stagenumber),
+        'pheadline': 'Filter: Stage ' + str(stagenumber),
+        'subjects': Subject.objects.all(),
+        'tasknumber': len(taskfilter)
+    }
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
 def subjects(request):
-	context = {
-		'ptitle': "Subject Overview",
-		'pheadline': "All subjects",
-		'subjects': Subject.objects.all()
-	}
-	return render(request, 'userinterface/overview.html', context)
+    context = {
+        'ptitle': 'Subject Overview',
+        'pheadline': 'All subjects',
+        'subjects': Subject.objects.all()
+    }
+    return render(request, 'userinterface/overview.html', context)
+
 
 @login_required
-def subject(request, subject):
-	taskfilter = Task.objects.filter(subject_id=Subject.objects.get(subject_name=subject))
-	context = {
-		'subject': subject,
-		'tasks': taskfilter,
-		'ptitle': "Filter: Subject " + subject,
-		'pheadline': "Filter: Subject " + subject,
-		'tasknumber': len(taskfilter)
-	}
-	return render(request, 'userinterface/taskview.html', context)
+def subject(request, subjectid):
+    subject = Subject.objects.get(subject_id=subjectid)
+    taskfilter = Task.objects.filter(subject=subject)
+
+    context = {
+        'subject': subject.subject_name,
+        'tasks': taskfilter,
+        'ptitle': 'Filter: Subject ' + subject.subject_name,
+        'pheadline': 'Filter: Subject ' + subject.subject_name,
+        'tasknumber': len(taskfilter)
+    }
+
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
-def stagesubject(request, stagenumber, subject):
-	taskfilter = Task.objects.filter(stage=stagenumber, subject_id=Subject.objects.get(subject_name=subject))
-	context = {
-		'subject': subject,
-		'tasks': taskfilter,
-		'ptitle': "Filter: Stage " + str(stagenumber) + " and subject " + subject,
-		'pheadline': "Filter: Stage " + str(stagenumber) + " and subject " + subject,
-		'tasknumber': len(taskfilter)
-	}
-	return render(request, 'userinterface/taskview.html', context)
+def stagesubject(request, stagenumber, subjectid):
+    subject = Subject.objects.get(subject_id=subjectid)
+    taskfilter = Task.objects.filter(stage=stagenumber, subject_id=subject)
+
+    context = {
+        'subject': subject.subject_name,
+        'tasks': taskfilter,
+        'ptitle': 'Filter: Stage ' + str(stagenumber) + ' and subject ' + subject.subject_name,
+        'pheadline': 'Filter: Stage ' + str(stagenumber) + ' and subject ' + subject.subject_name,
+        'tasknumber': len(taskfilter)
+    }
+
+    return render(request, 'userinterface/taskview.html', context)
+
 
 @login_required
-def details(request, id):
-	task = Task.objects.get(id=id)
-	context = {
-		'task': task,
-		'ptitle': "Task - " + task.task_name,
-		'pheadline': "Task " + task.task_name,
-		'owner': [ 1 if str(task.teacher) == str(request.user.username) else 0 ]
-	}
-	return render(request, 'userinterface/details.html', context)
+def details(request, taskid):
+    task = Task.objects.get(task_id=taskid)
+
+    context = {
+        'task': task,
+        'ptitle': 'Task - ' + task.task_name,
+        'pheadline': 'Task ' + task.task_name,
+        'owner': [True if str(task.teacher) == str(request.user.username) else False]
+    }
+
+    return render(request, 'userinterface/details.html', context)
+
 
 @login_required
 def download(request, path):
-	file_path = os.path.join(settings.MEDIA_ROOT, path)
-	if os.path.exists(file_path):
-		with open(file_path, 'rb') as file:
-			response = HttpResponse(file.read(), content_type="application/vnd.ms-excel")
-			response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-			return response
-	raise Http404("File does not exist.")
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            response = HttpResponse(file.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404('File does not exist.')
