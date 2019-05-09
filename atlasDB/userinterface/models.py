b9ac77d511b86d779eb2c5ea676b51e16e09e4b5
Conrad <grosserconrad@gmail.com>
2019-05-09 11:42:30 +0200

    Reworked models
    - Reworked models
    - Removed concept as it nears the beta

--- atlasDB/userinterface/models.py
+++ atlasDB/userinterface/models.py
@@ -2,22 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Subject(models.Model):
     subject_id = models.AutoField(primary_key=True)
-    subject_name = models.CharField(max_length=25, unique=True)
+    subject_name = models.CharField(max_length=25, unique=True, verbose_name='Name des Fachs')
 
     def __str__(self):
         return self.subject_name
 
+    class Meta:
+        verbose_name = "Fach"
+        verbose_name_plural = "Fächer"
+
+
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
-    task_name = models.CharField(max_length=255)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    last_changed_at = models.DateTimeField(auto_now=True)
-    document = models.FileField(upload_to='Tasks/%Y/%m')
-    teacher = models.ForeignKey(User, related_name="teacher", on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING, null=True, blank=True)
-    stage = models.IntegerField(validators=[MaxValueValidator(13), MinValueValidator(1)])
+    task_name = models.CharField(max_length=255, verbose_name='Aufgabe')
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name='Hochgeladen')
+    last_changed_at = models.DateTimeField(auto_now=True, verbose_name='Zuletzt geändert')
+    document = models.FileField(upload_to='Tasks/%Y/%m', verbose_name='Dateipfad')
+    teacher = models.ForeignKey(User, related_name="teacher", on_delete=models.CASCADE, verbose_name='Lehrer')
+    subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING, null=True, blank=True, verbose_name='Fach')
+    stage = models.IntegerField(validators=[MaxValueValidator(13), MinValueValidator(1)], verbose_name='Klassenstufe')
 
     def __str__(self):
         return self.task_name
+
+    class Meta:
+        verbose_name = "Aufgabe"
+        verbose_name_plural = "Aufgaben"
